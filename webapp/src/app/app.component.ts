539faaf92b2c26a5abdb32aa30eab1bfa515fed3
davikrig-ha <daviluia2@gmail.com>
2022-04-20 11:29:32 -0300

    ajusts

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver, LayoutModule } from '@angular/cdk/layout';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'webapp';
+  title = 'esigi-config';
+  activeMenu!: '';
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  menuList = [
+    {
+      name: 'Home',
+      icon: 'home',
+      selected: false,
+      action: 'home',
+    },
+    {
+      name: 'Active Directory',
+      icon: 'admin_panel_settings',
+      selected: false,
+      action: '',
+    },
+    {
+      name: 'E-mail',
+      icon: 'email',
+      selected: false,
+      action: '',
+    },
+    {
+      name: 'Cadastros',
+      icon: 'settings_applications',
+      selected: false,
+      action: '',
+    },
+  ];
+
+  collaboratorId!: string | null;
+
+  constructor(
+    private observer: BreakpointObserver,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((res: any) => {
+        this.activeMenu = res.url.split('/')[1];
+      });
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.observer.observe(['(max-width: 800px)']).subscribe((res: any) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+    }, 50);
+  }
+
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
 }
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { HttpClientModule } from '@angular/common/http';
+import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatButtonModule,
+    MatDividerModule,
+    HttpClientModule,
+    MatListModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
