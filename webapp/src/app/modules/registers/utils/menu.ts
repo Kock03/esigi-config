458e5d982a7603d0b8cf3045bbf88577e2e35541
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2022-11-08 17:14:50 -0300

    add

--- webapp/src/app/modules/registers/utils/menu.ts
+++ webapp/src/app/modules/registers/utils/menu.ts
@@ -5,17 +5,17 @@ export const TAB_MENU_LIST = [
       {
         content: 'Cadastro de Clientes',
         context: 'customer',
-        photo: 'src/../assets/icons/register.png'
+        photo: 'src/../assets/icons/customer-config.png'
       },
       {
         content: 'Cadastro de Collaborador',
         context: 'collaborator',
-        photo: 'src/../assets/icons/register.png',
+        photo: 'src/../assets/icons/collaborator-config.png',
       },
       {
         content: 'Cadastro de Projetos',
         context: 'project',
-        photo: 'src/../assets/icons/register.png',
+        photo: 'src/../assets/icons/projects-config.png',
       },
 
     ],
@@ -26,12 +26,12 @@ export const TAB_MENU_LIST = [
       {
         content: 'Cadastro de Fincanceiro',
         context: 'finance',
-        photo: 'src/../assets/icons/register.png',
+        photo: 'src/../assets/icons/finance-config.png',
       },
       {
         content: 'Cadastro de Genéricos',
         context: 'generic',
-        photo: 'src/../assets/icons/register.png',
+        photo: 'src/../assets/icons/generic-config.png',
       },
     ],
   },
