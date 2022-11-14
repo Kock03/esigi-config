4f14620d0267a067e547963aea78cdca7873f712
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2022-11-14 17:09:22 -0300

    'add'

--- webapp/src/app/modules/registers/utils/people-management-config.ts
+++ webapp/src/app/modules/registers/utils/people-management-config.ts
@@ -24,7 +24,7 @@ export const TAB_PEOPLE_MANAGEMENT_MENU_LIST = [
         context: 'collaborator',
         key: 'marital_status',
         icon: 'supervisor_account',
-        photo: 'src/../assets/icons/marital-status-config',
+        photo: 'src/../assets/icons/marital-status-config.png',
       },
     ]
   },
